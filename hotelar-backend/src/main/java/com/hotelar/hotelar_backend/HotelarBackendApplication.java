c6b5e4abb97906ac59a835d2c3d3d39a241d06e6
DevDeivy <deivylujan@hotmail.com>
2025-08-27 17:05:48 -0500

    :art: structure added

---
+++ hotelar-backend/src/main/java/com/hotelar/hotelar_backend/HotelarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hotelar.hotelar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelarBackendApplication.class, args);
+	}
+
+}
---
+++ hotelar-backend/src/test/java/com/hotelar/hotelar_backend/HotelarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelar.hotelar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

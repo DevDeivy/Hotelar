e027f6a8fe0c00ae92d910dae2553947abf04596
Daniel Andres Cabana Trejos <danielcabana0727@gmail.com>
2025-09-02 16:59:39 -0500

    :construction: [WORKING] styles and some functionalities need to be added

--- hotelar-frontend/src/app/authentication/rout.ts
+++ hotelar-frontend/src/app/authentication/rout.ts
@@ -1,6 +1,7 @@
 import { Register } from "./register/register";
 import { Routes } from "@angular/router";
 import { Login } from "./login/login";
+import { Home } from "../home/home";
 
 export const rout: Routes = [
     {
@@ -10,6 +11,10 @@ export const rout: Routes = [
     {
         path: 'login',
         component: Login
+    },
+    {
+        path: 'home',
+        component: Home
     },
     {
         path: '',
